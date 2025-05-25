19cefeaebcf2e12a83afb77f24e1b59489e633db
castroogbs <dev.gbcproject@gmail.com>
2025-05-24 23:09:10 -0300

    aula 8

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,20 @@
-FROM node:20
+# Etapa de build
+FROM node:20 AS build
 WORKDIR /app
 COPY package*.json ./
-RUN npm install
+RUN npm install --production
 COPY . .
 RUN npx prisma generate
+# RUN npx swagger-jsdoc -d src/swagger.ts -o dist/swagger.json
+RUN npm run build
+
+# Etapa de deploy
+FROM node:20
+WORKDIR /app
+COPY --from=build /app/dist ./dist
+COPY --from=build /app/node_modules ./node_modules
+COPY --from=build /app/prisma ./prisma
+COPY .env ./
+COPY --from=build /app/src ./src
 EXPOSE 3333
-CMD ["npm", "run", "dev"]
+CMD ["sh", "-c", "npx prisma migrate deploy && node dist/index.js"]
--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,14 @@ import categoriasRouter from "./routes/categorias";
 import authRouter from "./routes/auth";
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./swagger";
+import path from "path";
 
 
 dotenv.config();
 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3333;
 
 
 app.use(cors());
@@ -26,6 +27,12 @@ app.use("/categorias", categoriasRouter);
 app.use("/auth", authRouter);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+// if (process.env.NODE_ENV === "production") {
+//   const swaggerDocument = require(path.join(__dirname, 'swagger.json'));
+//   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// } else {
+//   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// }
 
 // Primeira rota de teste:
 app.get("/", (req: Request, res: Response) => {
